40a8d1d0e081013c9770651280d9a7e831ff3b10
lemancebrayilova <cbrayilovalman33@gmail.com>
2025-08-22 23:58:05 +0300

    Add footer user info and logout

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -3,6 +3,13 @@ import { sidebarItems } from '~/constants';
 import { cn } from '~/lib/utils';
 
 const NavItems = () => {
+
+    const user = {
+        name: 'Adrian',
+        email: 'contact@codemaker.pro',
+        imageUrl: '/assets/images/david.webp'
+    }
+
     return (
         <section className="nav-items">
             <Link to='/' className="link-logo">
@@ -32,6 +39,19 @@ const NavItems = () => {
                         </NavLink>
                     ))}
                 </nav>
+
+                <footer className="nav-footer">
+                    <img src={user?.imageUrl || '/assets/images/david.webp'} alt={user?.name || 'David'} />
+
+                    <article>
+                        <h2>{user?.name}</h2>
+                        <p>{user?.email}</p>
+                    </article>
+
+                    <button onClick={ () => { console.log( 'logout' )} } className="cursor-pointer">
+                        <img src="/assets/icons/logout.svg" alt="logout" className='size-6' />
+                    </button>
+                </footer>
             </div>
         </section>
     )
