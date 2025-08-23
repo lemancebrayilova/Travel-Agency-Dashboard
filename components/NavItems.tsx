dc668024322b597662f6f8ec41fb658b521039ef
lemancebrayilova <cbrayilovalman33@gmail.com>
2025-08-24 02:01:12 +0300

    Add toggleable mobile sidebar with NavItems

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -2,7 +2,7 @@ import { Link, NavLink } from 'react-router';
 import { sidebarItems } from '~/constants';
 import { cn } from '~/lib/utils';
 
-const NavItems = () => {
+const NavItems = ({ handleClick }: { handleClick?: () => void }) => {
 
     const user = {
         name: 'Adrian',
@@ -24,7 +24,7 @@ const NavItems = () => {
                             {({ isActive }: { isActive: boolean }) => (
                                 <div className={cn( 'group nav-item', {
                                     'bg-primary-100 !text-white': isActive
-                                })}>
+                                })} onClick={handleClick}>
 
                                     <img 
                                         src={icon}
