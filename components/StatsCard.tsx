55768a86cecdba78330178332fe7c146f2bc77e3
lemancebrayilova <cbrayilovalman33@gmail.com>
2025-08-24 14:15:44 +0300

    Add StatsCard and TripCard components

---
+++ components/StatsCard.tsx
@@ -0,0 +1,28 @@
+import { calculateTrendPercentage } from "~/lib/utils"
+
+const StatsCard = ({
+    headerTitle,
+    total,
+    lastMonthCount,
+    currentMonthCount }: StatsCard) => {
+    
+    const { trend, percentage } = calculateTrendPercentage(currentMonthCount, lastMonthCount);
+
+    const isDecrement = trend === 'decrement';
+        
+    return (
+        <article className="stats-card">
+            <h3 className="text-base font-medium">
+                {headerTitle}
+            </h3>
+
+            <div className="content">
+                <div className="flex flex-col gap-4">
+                    <h2 className="text-4xl font-semibold">{total}</h2>
+                </div>
+            </div>
+        </article>
+    )
+}
+
+export default StatsCard
