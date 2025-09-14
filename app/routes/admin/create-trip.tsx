4f88d477baef25cbd76a49ec853b8879b504a74e
lemancebrayilova <cbrayilovalman33@gmail.com>
2025-09-14 22:37:22 +0300

    Fetch countries and display with flags

--- app/routes/admin/create-trip.tsx
+++ app/routes/admin/create-trip.tsx
@@ -1,11 +1,66 @@
-import { Header } from "../../../components"
+import { Header } from "../../../components";
+import { ComboBoxComponent } from "@syncfusion/ej2-react-dropdowns";
+import type { Route } from "./+types/create-trip";
 
-const CreateTrip = () => {
-    return (
-        <main className="flex flex-col gap-10 pb-20 wrapper">
-            <Header title="Add a New Trip" description="View and edit AI Generated travel plans" />
-        </main>
-    )
+export const loader = async () => {
+    const response = await fetch('https://www.apicountries.com/countries');
+
+    const data = await response.json(); 
+
+    return data.map((country: any) => ({ 
+        name: country.name,
+        flag: country.flags.png,
+        coordinates: country.latlng, 
+        value: country.name, 
+        openStreetMap: country.maps?.openStreetMap, 
+    }));
 }
+    
+const CreateTrip = ({ loaderData }: Route.ComponentProps) => { 
+    const handleSubmit = async () => {}; 
+    const handleChange = (key: keyof TripFormData, value: string | number) => {};
 
+    const countries = loaderData as Country[];
+
+    const countryData = countries.map((country) => ({ 
+        text: country.name,
+        value: country.value,
+        flag: country.flag
+    })) 
+
+    return ( 
+        <main className="flex flex-col gap-10 pb-20 wrapper"> 
+            <Header title="Add a New Trip" description="View and edit AI Generated travel plans" />
+
+            <section className="mt-2.5 wrapper-md"> 
+                <form className="trip-form" onSubmit={handleSubmit}> 
+                    <div> 
+                        <label htmlFor="country"> Country </label> 
+
+                        <ComboBoxComponent 
+                            id="country" 
+                            dataSource={countryData} 
+                            fields={{ text: 'text', value: 'value' }} 
+                            placeholder="Select a Country" 
+                            className="combo-box"
+                            itemTemplate={(data: any) => (
+                                <div className="flex items-center gap-2">
+                                    <img src={data.flag} alt={data.text} className="w-4 h-3 object-cover" />
+                                    <span>{data.text}</span>
+                                </div>
+                            )}
+                            change = {(e: { value: string | undefined }) => {
+                                if(e.value) {
+                                    handleChange('country', e.value);
+                                }
+                            }}
+                            allowFiltering
+                        /> 
+                    </div> 
+                </form> 
+            </section> 
+        </main> 
+    ) 
+} 
+    
 export default CreateTrip
