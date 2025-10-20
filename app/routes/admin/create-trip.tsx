ccb31c530d8b8e5aff5971cc10db6481fffb4453
lemancebrayilova <cbrayilovalman33@gmail.com>
2025-10-20 23:00:38 +0300

    Generate Trip using AI

--- app/routes/admin/create-trip.tsx
+++ app/routes/admin/create-trip.tsx
@@ -1,13 +1,14 @@
 import { Header } from "../../../components";
 import { ComboBoxComponent } from "@syncfusion/ej2-react-dropdowns";
 import type { Route } from "./+types/create-trip";
-import { comboBoxItems, groupTypes, interests, selectItems, travelStyles } from "~/constants";
+import { comboBoxItems, selectItems } from "~/constants";
 import { cn, formatKey } from "~/lib/utils";
 import { LayerDirective, LayersDirective, MapsComponent } from "@syncfusion/ej2-react-maps";
 import { useState } from "react";
 import { world_map } from "~/constants/world_map";
 import { ButtonComponent } from "@syncfusion/ej2-react-buttons";
 import { account } from "~/appwrite/client";
+import { useNavigate } from "react-router";
 
 export const loader = async () => {
     const response = await fetch('https://www.apicountries.com/countries');
@@ -25,6 +26,7 @@ export const loader = async () => {
     
 const CreateTrip = ({ loaderData }: Route.ComponentProps) => { 
     const countries = loaderData as Country[];
+    const navigate = useNavigate();
 
     const [formData, setFormData] = useState<TripFormData>({
         country: countries[0]?.name || '',
@@ -80,6 +82,11 @@ const CreateTrip = ({ loaderData }: Route.ComponentProps) => {
                     userId: user.$id
                 })
             })
+        
+            const result: CreateTripResponse = await response.json();
+
+            if (result?.id) navigate(`/trips/${result.id}`)
+            else console.error('Failed to generate a trip')
         } catch (e) {
             console.error('Error generating trip', e);
         } finally {
